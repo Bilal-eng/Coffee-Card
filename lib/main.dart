45a198b69cb738c0cdea06a3734ba00ba5d73c8d
Bilal-PC <bilal-pc@Bilal-PCs-MacBook-Pro.local>
2025-05-16 10:38:56 +0300

    Add MaterialApp & Scaffold widgets

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Coffee Id'),
+          backgroundColor: Colors.brown[700],
+          centerTitle: true,
+        ),
+        body: Text('Hello, Ninjas'),
+      ),
+    ),
+  );
+}
